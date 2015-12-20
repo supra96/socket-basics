6ca75d9a8a183121652a6bd8b65b4ac2d6a35aaf
supratik goswami <supratik goswami>
2015-12-20 10:34:54 -0500

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,11 @@
+//this is front end code
+
 var socket=io(); //io isnt created by you, its defined
                      // by socket io library
 socket.on('connect', function(){
 	console.log('Connected to soxket.io server');
+});
+socket.on('message',function (message){
+console.log('New message');
+console.log(message.text);
 });
--- server.js
+++ server.js
@@ -6,8 +6,18 @@ var io=require('socket.io')(http);
 
 app.use(express.static(__dirname+'/public'));
 
-io.on('connection', function(){
+io.on('connection', function (socket) {//if you put socket param in function, it refers to individual socket
 	console.log('user connected via socket.io');
+	socket.on('message', function (message){
+		console.log('Message received: '+ message.text)
+		//io.emit - sends it to everyone including the one who sent it
+		socket.broadcast.emit('message',message);//sends the message to everyboday but the one who sent it
+	});
+
+	socket.emit('message',{
+		text:'Welcome to the chat application!'
+
+	});
 }); //listin for events
 
 http.listen(PORT, function () {
